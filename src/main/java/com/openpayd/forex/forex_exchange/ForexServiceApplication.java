7019bd44e6456f81a924ad9442dd9ea896962197
Emre Bülbül <87551092+Misleadinge@users.noreply.github.com>
2025-03-07 06:17:44 +0300

    initialize springboot
    h2 Database, Spring Web, Lombok, OpenFeign, Spring Data JPA, Spring Boot Devtools added as dependencies from spring initializr

---
+++ src/main/java/com/openpayd/forex/forex_exchange/ForexServiceApplication.java
@@ -0,0 +1,13 @@
+package com.openpayd.forex.forex_exchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForexServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForexServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/openpayd/forex/forex_exchange/ForexServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openpayd.forex.forex_exchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForexServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
